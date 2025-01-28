c86c891367db6d5f2e2013f7440e7f6889f0c5ab
Asmit Deshmukh <asmit.deshmukh1704@gmail.com>
2025-01-28 23:48:43 +0530

    timeline: Add timeline component

--- frontend/src/components/Timeline.jsx
+++ frontend/src/components/Timeline.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import { content } from "../constants/Timeline";
+import { Circle } from "lucide-react";
+import { motion } from "framer-motion";
 
 export default function Timeline() {
     return(
-         <div>Timeline</div>
+        <>
+            <div className="justify-center ">
+                {content.map((content) => (
+                    <div className="w-full px-16 flex flex-row justify-center">
+                        
+                        {/* Title and Description */}
+                        <motion.div  
+                        initial={{opacity:0}}
+                        transition={{delay: (content.id - 1)*0.75, duration: 1}}
+                        animate={{opacity: 1 }}
+                        className={`basis-2/5 p-8 ${(content.id%2)===0 ? "order-3" : "order-1" }`}>
+                            <div className={`font-bold text-red-500 text-3xl ${(content.id%2)===0 ? "justify-self-start": "justify-self-end" }`}>{content.title}</div>
+                            <div className={`${(content.id%2)===0 ? "justify-self-start text-left": "justify-self-end text-right" } text-slate-600 text-l`}>{content.description}</div>
+                        </motion.div>
+                        
+                        {/* Create Vertical Line */}
+                        <div className="relative basis-20px self-auto order-2"> 
+                            <motion.div 
+                            initial={{height: 0, opacity:0 }}
+                            transition={{delay: (content.id - 1)*0.75, duration: 1}}
+                            animate={{height: "100%", opacity: 1 }}
+                            className={`absolute h-full ${content.id===1 ? "top-10" : ""} w-0 left-1/2 border border-red-200`}></motion.div>
+                            <motion.div
+                            initial={{opacity:0}}
+                            transition={{delay: (content.id - 1)*0.75, duration: 1}}
+                            animate={{opacity: 1 }}
+                            className="z-1 pt-8"><Circle style={{color: "#e0761f", backgroundColor: "white"}} size={20} /></motion.div>
+                        </div>
+                        
+                        {/* Create Date and Year Element */}
+                        <motion.div
+                        initial={{opacity:0}}
+                        transition={{delay: (content.id - 1)*0.75, duration: 1}}
+                        animate={{opacity: 1 }}
+                        className={`basis-2/5 p-8 ${(content.id%2)===0 ? "order-1" : "order-3" }`}>
+                            <div className={`font-bold text-red-500 text-xl ${(content.id%2)===0 ? "justify-self-end": "justify-self-start" }`}>2025</div>
+                            <div className={`${(content.id%2)===0 ? "justify-self-end": "justify-self-start" } text-slate-600 text-m`}>{content.date}</div>
+                        </motion.div>
+                    </div>
+                ))}
+            </div>
+        </>
     );
 }
---
+++ frontend/src/constants/Timeline.jsx
@@ -0,0 +1,21 @@
+
+export const content = [
+    {
+        id: 1,
+        title: "Title 1",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, eum sit? Asperiores, magnam sed nisi quos, ea voluptatum, accusantium beatae odit necessitatibus sit corrupti libero dolor veniam. Ea, labore enim. ",
+        date: "22 Jan"
+    },
+    {
+        id: 2,
+        title: "Title 2",
+        description: " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus nobis beatae sapiente nisi tempora facilis temporibus et libero accusantium illum impedit laborum inventore porro, commodi excepturi reprehenderit voluptatem, at nihil? ",
+        date: "22 Jan"
+    },
+    {
+        id: 3,
+        title: "Title 3",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ad doloremque iste nemo a at quo ipsam non blanditiis sint, expedita delectus cupiditate. Quisquam, perferendis sed mollitia doloribus reprehenderit iusto. ",
+        date: "22 Jan"
+    }
+]
