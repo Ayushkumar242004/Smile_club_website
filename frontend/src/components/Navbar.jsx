4e06be1a3020290c9d52fbc7c85146b51f6b8d4d
AbhinayaSrinivas <abhinayasrinivas30@gmail.com>
2025-01-29 13:08:06 +0530

    Added Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,72 @@
-import React from "react";
+// import React from "react";
+// import '../App.css'
+// import { useState } from "react";
+// import { FaBars, FaTimes } from "react-icons/fa";
+// import { Link } from "react-router-dom";
+   
 
-export default function Navbar() {
-	return(
-         <div>Navbar</div>
-    );
-}
+// export default function Navbar() {
+//     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  
+//     const toggleMenu = () => {
+//       setIsMobileMenuOpen(!isMobileMenuOpen);
+//     };
+//     return (
+//         <header className="navbar-header">
+//           <div className="lo_go">
+//             <img src="/logo.png" alt="Smile Club Logo" />
+//           </div>
+//           <div className="navbar">
+//             <nav className={isMobileMenuOpen ? "menu open" : "menu"}>
+//               <ul>
+//                   <li><Link to="/home">Home</Link></li>
+//                   <li><Link to="/blog">Blog</Link></li>
+//                   <li><Link to="/event">Events</Link></li>                
+//                   <li><Link to="/team">Team</Link></li>
+//               </ul>
+//             </nav>
+//             <div className="hamburger" onClick={toggleMenu}>
+//               {isMobileMenuOpen ? <FaTimes /> : <FaBars />}
+//             </div>
+//           </div>
+//         </header>
+//       );
+//     }
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import '../navbar.css' 
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <header className="navbar-header">
+      <div className="lo_go">
+          <img src="/logo.png" alt="Logo" />
+      </div>
+      <nav className="navbar">
+        
+        
+        
+
+        <ul className={`menu ${isOpen ? 'open' : ''}`}>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/blogs">Blog</Link></li>
+          <li><Link to="/events">Events</Link></li>
+          <li><Link to="/team">Team</Link></li>
+        </ul>
+
+        <div className="hamburger" onClick={toggleMenu}>
+          {isOpen ? <FaTimes /> : <FaBars />}
+        </div>
+
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,10 +4,12 @@ import AboutUs from "../components/AboutUs";
 import Vision from "../components/Vision";
 import EventsCard from "../components/EventsCard";
 import Marquee from "../components/Marquee";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
     <div className="">
+      <Navbar/>
       <Hero />
       <AboutUs />
       <Vision />
