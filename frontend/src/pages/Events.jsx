d34016d4e169930380c7b88975c385fa811382ab
Ayushkumar242004 <143803366+Ayushkumar242004@users.noreply.github.com>
2025-02-01 14:34:48 +0530

    added broadcast

--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import EventsCard from "../components/EventsCard";
-
+import Broadcast from "../components/Broadcast"
 export default function Events() {
   return (
     <div className="">
       <EventsCard />    
+      <Broadcast/>
     </div>
   );
 }
