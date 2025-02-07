fcc970f6d2f0a8766fdbf28ee676d71815d0224f
Asmit Deshmukh <asmit.deshmukh1704@gmail.com>
2025-02-07 23:05:13 +0530

    faq: Add FAQ component

---
+++ frontend/src/constants/FAQ.jsx
@@ -0,0 +1,50 @@
+export const faqs = [
+    {
+    id: 1,
+    question: "What is a Temple?",
+    answer:
+        "Temple is a place where Hindu worship our Bhagwan Ram, Shiva, Vishnu, Krishna etc. Proin eget tortor risus. Vivamus magna justo. People ask questions related to Hinduism",
+    },
+    {
+    id: 2,
+    question: "Getting Started with an Temple",
+    answer:
+        "Temple is a place where Hindu worship our Bhagwan Ram, Shiva, Vishnu, Krishna etc. Proin eget tortor risus. Vivamus magna justo. People ask questions related to Hinduism",
+    },
+    {
+    id: 3,
+    question: "Do I have the latest version?",
+    answer:
+        "Temple is a place where Hindu worship our Bhagwan Ram, Shiva, Vishnu, Krishna etc. Proin eget tortor risus. Vivamus magna justo. People ask questions related to Hinduism",
+    },
+    {
+    id: 4,
+    question: "How can I change header",
+    answer:
+        "Temple is a place where Hindu worship our Bhagwan Ram, Shiva, Vishnu, Krishna etc. Proin eget tortor risus. Vivamus magna justo. People ask questions related to Hinduism",
+    },
+    {
+    id: 5,
+    question: "How many times can I use a Temple?",
+    answer:
+        "Temple is a place where Hindu worship our Bhagwan Ram, Shiva, Vishnu, Krishna etc. Proin eget tortor risus. Vivamus magna justo. People ask questions related to Hinduism",
+    },
+    {
+    id: 6,
+    question: "How to migrate my website?",
+    answer:
+        "Temple is a place where Hindu worship our Bhagwan Ram, Shiva, Vishnu, Krishna etc. Proin eget tortor risus. Vivamus magna justo. People ask questions related to Hinduism",
+    },
+    {
+    id: 7,
+    question: "How to migrate my website?",
+    answer:
+        "Temple is a place where Hindu worship our Bhagwan Ram, Shiva, Vishnu, Krishna etc. Proin eget tortor risus. Vivamus magna justo. People ask questions related to Hinduism",
+    },
+    {
+    id: 8,
+    question: "Changing the site footer",
+    answer:
+        "Temple is a place where Hindu worship our Bhagwan Ram, Shiva, Vishnu, Krishna etc. Proin eget tortor risus. Vivamus magna justo. People ask questions related to Hinduism",
+    },
+];
