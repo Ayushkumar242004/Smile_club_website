86b41ccfda4f43f26d3762df85d432182fe4eeb9
Ayushkumar242004 <143803366+Ayushkumar242004@users.noreply.github.com>
2025-02-09 21:13:14 +0530

    Marquee done

--- frontend/src/pages/Team.jsx
+++ frontend/src/pages/Team.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import OurTeam from "../components/OurTeam";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default function Team() {
   return (
-    <div className="">
-      <OurTeam />    
+    <div className="bg-red-200 min-h-screen">
+      <Navbar />
+      <OurTeam />
+      <Footer />
     </div>
   );
 }
