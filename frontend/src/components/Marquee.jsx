143f6a881b0b49a71ff18bf7b0b9aec618569325
Ayushkumar242004 <143803366+Ayushkumar242004@users.noreply.github.com>
2025-02-11 16:51:10 +0530

    Marquee corrected

--- frontend/src/components/Marquee.jsx
+++ frontend/src/components/Marquee.jsx
@@ -4,13 +4,13 @@ export default function Marquee() {
   return (
     <div className="relative overflow-hidden h-24 mb-8 border-y-2 border-red-700 w-full">
       {/* Marquee Strip */}
-      <div className="flex items-center h-24 px-8 bg-[#FBFEF9] shadow-lg w-full animate-scroll">
+      <div className="flex items-center h-24 px-8 bg-[#FBFEF9]  w-full animate-scroll">
         <div className="flex whitespace-nowrap">
-          <span className="mr-8 text-[#770707] font-bold text-2xl text-shadow-md">
+          <span className="mr-8 text-[#770707] font-bold text-2xl ">
             "You have the right to work, but never to the fruit of work. Let not the fruits of action be your motive, nor let your attachment be to inaction."
             (तुम्हारा कर्तव्य कार्य करना है, परंतु उसके फल का अधिकार तुम्हारा नहीं है। तुम्हारे कार्य के फलों पर तुम्हारा कोई अधिकार नहीं होना चाहिए, और न ही तुम्हें निष्क्रियता में आसक्ति होनी चाहिए।)
           </span>
-          <span className="mr-8 text-[#770707] font-extrabold text-2xl text-shadow-md">
+          <span className="mr-8 text-[#770707] font-extrabold text-2xl ">
             "You have the right to work, but never to the fruit of work. Let not the fruits of action be your motive, nor let your attachment be to inaction."
             (तुम्हारा कर्तव्य कार्य करना है, परंतु उसके फल का अधिकार तुम्हारा नहीं है। तुम्हारे कार्य के फलों पर तुम्हारा कोई अधिकार नहीं होना चाहिए, और न ही तुम्हें निष्क्रियता में आसक्ति होनी चाहिए।)
           </span>
@@ -34,9 +34,7 @@ export default function Marquee() {
             animation: scroll 20s linear infinite;
             width: max-content;
           }
-          .text-shadow-md {
-            text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
-          }
+          
         `}
       </style>
     </div>
