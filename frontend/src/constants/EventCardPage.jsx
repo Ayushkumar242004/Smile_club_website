9bd976aa16c41e0b26c9006d1daab288f99b5988
Asmit Deshmukh <asmit.deshmukh1704@gmail.com>
2025-02-27 13:00:54 +0530

    CardPage: add social media links and information to constants

--- frontend/src/constants/EventCardPage.jsx
+++ frontend/src/constants/EventCardPage.jsx
@@ -25,11 +25,51 @@ export const events = [
         title: "Should I Believe in Rebirth?",
         points: ["People ask questions related to Hinduism","We never believe in reborn","People ask questions related to Hinduism", "We never believe in reborn"]
     },
-    tags: ["Hinduism", "Temple", "Faith"]
+    tags: ["Hinduism", "Temple", "Faith"],
+    date: "25 September, 2018",
+    time: "(8:00 am - 9:00 am)",
+    category: "Temple",
+    organizer: "Event Planer",
+    phone: "+02 1565 1236",
+    email: "supportmail@gmail.com",
     },
     {
-        id: 2,
-        title: "Maha Shivaratri Celebration!",
+    id: 2,
+    title: "Maha Shivaratri Celebration!",
+    images: ["/CardPage/shiva1.jpg", "/CardPage/shiva2.jpg", "/CardPage/shiva3.jpg"],
+    qna: [
+        {
+            question: "I Will Not Reject My Faith in Rama, Shiva, Vishnu, Krishna etc.",
+            answer: "Temple is a place where Hindu worship our Bhagwan Ram, Shiva, Vishnu, Krishna etc. Proin eget tortor industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type, People ask questions related to Hinduism. specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+        },
+        {
+            question: "Our mission is to share the Good of Hinduism",
+            answer: "I find this argument as sad as the argument that without a belief in God people would likewise be unethical. Our mission is to share the Good of Hinduism, Loving, Faith and Serving. People ask questions related to Hinduism. to make a type specimen book.",
+        },
+        {
+            question: "People ask questions related to Hinduism",
+            answer: "Our mission is to share the Good of Hinduism, Loving, Faith and Serving. People ask questions related to Hinduism. Temple is a place where Hindu worship our Bhagwan Ram, Shiva, Vishnu, Krishna etc. galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting",
+        },
+        {
+            question: "Should I Believe in Rebirth?",
+            answer: "We never believe in reborn the Good of Hinduism, Loving, Faith and Serving. People ask questions related to Hinduism. Temple is a place where Hindu worship our Bhagwan Ram, Shiva, Vishnu, Krishna etc. galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        }
+    ],
+    points: {
+        title: "Should I Believe in Rebirth?",
+        points: ["People ask questions related to Hinduism","We never believe in reborn","People ask questions related to Hinduism", "We never believe in reborn"]
+    },
+    tags: ["Hinduism", "Temple", "Faith"],
+    date: "25 September, 2018",
+    time: "(8:00 am - 9:00 am)",
+    category: "Temple",
+    organizer: "Event Planer",
+    phone: "+02 1565 1236",
+    email: "supportmail@gmail.com",
+        },
+    {
+        id: 3,
+        title: "AYODHYA RAM PUNAH AAGMAN",
         images: ["/CardPage/shiva1.jpg", "/CardPage/shiva2.jpg", "/CardPage/shiva3.jpg"],
         qna: [
             {
@@ -53,34 +93,12 @@ export const events = [
             title: "Should I Believe in Rebirth?",
             points: ["People ask questions related to Hinduism","We never believe in reborn","People ask questions related to Hinduism", "We never believe in reborn"]
         },
-        tags: ["Hinduism", "Temple", "Faith"]
+        tags: ["Hinduism", "Temple", "Faith"],
+        date: "25 September, 2018",
+        time: "(8:00 am - 9:00 am)",
+        category: "Temple",
+        organizer: "Event Planer",
+        phone: "+02 1565 1236",
+        email: "supportmail@gmail.com",
         },
-        {
-            id: 3,
-            title: "AYODHYA RAM PUNAH AAGMAN",
-            images: ["/CardPage/shiva1.jpg", "/CardPage/shiva2.jpg", "/CardPage/shiva3.jpg"],
-            qna: [
-                {
-                    question: "I Will Not Reject My Faith in Rama, Shiva, Vishnu, Krishna etc.",
-                    answer: "Temple is a place where Hindu worship our Bhagwan Ram, Shiva, Vishnu, Krishna etc. Proin eget tortor industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type, People ask questions related to Hinduism. specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
-                },
-                {
-                    question: "Our mission is to share the Good of Hinduism",
-                    answer: "I find this argument as sad as the argument that without a belief in God people would likewise be unethical. Our mission is to share the Good of Hinduism, Loving, Faith and Serving. People ask questions related to Hinduism. to make a type specimen book.",
-                },
-                {
-                    question: "People ask questions related to Hinduism",
-                    answer: "Our mission is to share the Good of Hinduism, Loving, Faith and Serving. People ask questions related to Hinduism. Temple is a place where Hindu worship our Bhagwan Ram, Shiva, Vishnu, Krishna etc. galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting",
-                },
-                {
-                    question: "Should I Believe in Rebirth?",
-                    answer: "We never believe in reborn the Good of Hinduism, Loving, Faith and Serving. People ask questions related to Hinduism. Temple is a place where Hindu worship our Bhagwan Ram, Shiva, Vishnu, Krishna etc. galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-                }
-            ],
-            points: {
-                title: "Should I Believe in Rebirth?",
-                points: ["People ask questions related to Hinduism","We never believe in reborn","People ask questions related to Hinduism", "We never believe in reborn"]
-            },
-            tags: ["Hinduism", "Temple", "Faith"]
-            },
 ];
