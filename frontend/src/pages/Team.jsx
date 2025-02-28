84b1dfc5addea9080a90f2e70dbe5661d7b307a5
Ayushkumar242004 <143803366+Ayushkumar242004@users.noreply.github.com>
2025-03-01 03:21:11 +0530

    team fixed

--- frontend/src/pages/Team.jsx
+++ frontend/src/pages/Team.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 import OurTeam from "../components/OurTeam";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
 import Hero_Team from "../components/Hero_Team";
 
 export default function Team() {
   return (
     <div className="bg-red-200 min-h-screen">
-      {/* <Navbar /> */}
       <Hero_Team />
       <OurTeam />
-      {/* <Footer /> */}
     </div>
   );
 }
