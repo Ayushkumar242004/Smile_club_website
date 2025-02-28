6c6f27af6163ed01c5b35cf41a9b15b21910b941
Ayushkumar242004 <143803366+Ayushkumar242004@users.noreply.github.com>
2025-02-28 21:20:05 +0530

    timeline updated

--- frontend/src/components/Timeline.jsx
+++ frontend/src/components/Timeline.jsx
@@ -9,16 +9,16 @@ export default function Timeline() {
 
     return (
         <div className="justify-center my-16">
-            <div className="text-start mb-16 lg:px-28 md:px-16 sm:px-24 px-14">
+            <div className="text-start mb-16 lg:px-28 md:px-16 sm:px-28 px-14">
                 
                 <p className="uppercase text-[#8B1D42] md:text-2xl text-xl">ॐ</p>
-                <h2 className="lg:text-6xl text-4xl font-bold text-[#c52f2f]">Timeline</h2>
+                <h2 className="text-3xl sm:text-4xl lg:text-6xl font-bold text-[#c52f2f]">Timeline</h2>
                 <a href="/events" className="text-sm text-gray-600 hover:text-[#c52f2f] flex items-center lg:px-32 px-9 mt-2 ">
                     View All Events →
                 </a>
             </div>
             {content.map((content) => (
-                <div className="w-full px-16 flex flex-row justify-center" key={content.id}>
+                <div className="w-full px-8 flex flex-row justify-center" key={content.id}>
                     
                     {/* Title and Description */}
                     <motion.div
@@ -30,9 +30,9 @@ export default function Timeline() {
                         <motion.div
                             onHoverStart={() => setHover(content.id)}
                             onHoverEnd={() => setHover(0)}
-                            whileHover={{ scale: 1.15 }}
+                            whileHover={{ scale: 1.1 }}
                             transition={{ duration: 0.7 }}
-                            className={`font-bold text-[#E23D3D] text-3xl ${(content.id % 2) === 0 ? "justify-self-start" : "md:justify-self-end justify-self-start"}`}
+                            className={`font-bold text-[#E23D3D] text-1xl sm:text-2xl ${(content.id % 2) === 0 ? "justify-self-start" : "md:justify-self-end justify-self-start"}`}
                         >
                             {content.title}
                         </motion.div>
