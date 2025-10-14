89cf02b213b0a359fa651c5a608fe184515d4c00
JM-2004 <jyoti.231ee122@nitk.edu.in>
2025-10-14 23:52:31 +0530

    update TeamData

--- frontend/src/constants/TeamData.js
+++ frontend/src/constants/TeamData.js
@@ -1,21 +1,15 @@
 export const teamData = [
   //web team  
     {
-        name: "Krishna Tulsyan",
-        designation: "Web Head",
-        photo: "https://res.cloudinary.com/dqlgqfxi6/image/upload/v1743444565/krishna_vmgja7.jpg",
-        linkedin: "https://www.linkedin.com/in/krishnatulsyan/",
-        category: "Web Team"
-    },
-    {
       name: "Ayush Kumar",
-      designation: "Web Coordinator",
+      designation: "Web Head",
       photo: "https://res.cloudinary.com/dqlgqfxi6/image/upload/v1743444559/ayush_c8qigz.jpg",
       linkedin: "https://linkedin.com/in/ayush-kumar-5a2ab4299",
       category: "Web Team"
     },
     {
         name: "Anshi Sachan",
+        designation: "Web Coordinator",
         photo: "https://res.cloudinary.com/dqlgqfxi6/image/upload/v1743444555/anshi_aw9wdu.jpg",
         linkedin: "https://linkedin.com/in/anshi-sachan",
         category: "Web Team"
@@ -33,12 +27,6 @@ export const teamData = [
         category: "Web Team"
       },
       {
-        name: "Abhinaya Siripurapu ",
-        photo: "https://res.cloudinary.com/dqlgqfxi6/image/upload/v1743444551/abhinaya_dq3od4.jpg",
-        linkedin: "https://www.linkedin.com/in/abhinaya-srinivas-77b52225a/",
-        category: "Web Team"
-      },
-      {
         name: "Anvesha Agarwal",
         photo: "https://res.cloudinary.com/dqlgqfxi6/image/upload/v1743444556/anvesha_rxioxu.jpg",
         linkedin: "https://www.linkedin.com/in/anveshaagrawal18/",
@@ -50,95 +38,171 @@ export const teamData = [
       linkedin: "https://www.linkedin.com/in/yashwanth-r-bb437128a",
       category: "Web Team"
     },
+    {
+      name: "Sujan H",
+      photo: "https://res.cloudinary.com/dqlgqfxi6/image/upload/v1760465435/sujan_rkdxok.jpg",
+      linkedin: "https://www.linkedin.com/in/sujan-h-358068378",
+      category: "Web Team"
+    },
+    {
+      name: "Samarth Talawar",
+      photo: "https://res.cloudinary.com/dqlgqfxi6/image/upload/v1760465508/samarth_sx1bwx.jpg",
+      linkedin: "https://www.linkedin.com/in/samarth-talawar-1a4323369",
+      category: "Web Team"
+    },
+    {
+      name: "Vidith Venkatesha Murthy ",
+      photo: "https://res.cloudinary.com/dqlgqfxi6/image/upload/v1760465569/vidith_rupqja.jpg",
+      linkedin: "https://www.linkedin.com/in/vidith-venkatesha-murthy",
+      category: "Web Team"
+    },
+    
+
+
+
+  
     //core team
     {
-      name: "Raghav Bajpai",
+      name: "Krishna Tulsyan",
       designation: "Convenor",
-      photo: "https://res.cloudinary.com/dqlgqfxi6/image/upload/v1743444568/raghav_lqvitb.jpg",
-      linkedin: "https://www.linkedin.com/in/raghav-bajpai-b76a22223/",
+      photo: "https://res.cloudinary.com/dqlgqfxi6/image/upload/v1743444565/krishna_vmgja7.jpg",
+      linkedin: "https://www.linkedin.com/in/krishnatulsyan/",
       category: "Core Team"
     },
     {
-      name: "Kaveri G. H",
-      designation: "Joint Convenor",
-      photo: "https://res.cloudinary.com/dqlgqfxi6/image/upload/v1743444563/kaveri_xhnzwf.jpg",
-      linkedin: "https://www.linkedin.com/in/kaveri-g-h-05778722a/",
-      category: "Core Team"
-    },
-    {
-      name: "Harsh Narayankar",
-      designation: "Secretary",
-      photo: "https://res.cloudinary.com/dqlgqfxi6/image/upload/v1743444560/harsh_pnsjxd.jpg",
-      linkedin: "https://www.linkedin.com/in/harshnarayankar/",
-      category: "Core Team"
-    },
-    {
-      name: "Sheethal Pai",
-      designation: "Content Head",
-      photo: "https://res.cloudinary.com/dqlgqfxi6/image/upload/v1743444570/sheethal_auwzfi.jpg",
-      linkedin: "https://www.linkedin.com/in/sheethal-pai-520375327/",
-      category: "Core Team"
-    },
-    {
-        name: "Krishna Tulsyan",
-        designation: "Web Head",
-        photo: "https://res.cloudinary.com/dqlgqfxi6/image/upload/v1743444565/krishna_vmgja7.jpg",
-        linkedin: "https://www.linkedin.com/in/krishnatulsyan/",
-        category: "Core Team"
-    },
-    {
-      name: "Ayush Kumar",
-      designation: "Web Coordinator",
-      photo: "https://res.cloudinary.com/dqlgqfxi6/image/upload/v1743444559/ayush_c8qigz.jpg",
-      linkedin: "https://linkedin.com/in/ayush-kumar-5a2ab4299",
-      category: "Core Team"
-    },
-    {
-      name: "Yash Kumar Poddar",
-      designation: "Event Head ",
-      photo: "https://res.cloudinary.com/dqlgqfxi6/image/upload/v1743444575/yash_dmjsqx.jpg",
-      linkedin: "https://www.linkedin.com/in/yashkumar114/",
-      category: "Core Team"
-    },
-    {
-      name: "Abhinaya Siripurapu ",
-      designation: "Publicity Head",
+      name: "Abhinaya Siripurapu",
+      designation: "Vice Convenor",
       photo: "https://res.cloudinary.com/dqlgqfxi6/image/upload/v1743444551/abhinaya_dq3od4.jpg",
       linkedin: "https://www.linkedin.com/in/abhinaya-srinivas-77b52225a/",
       category: "Core Team"
     },
     {
-      name: "Nishadeep H ",
-      designation: "Event & Publicity Coordinator ",
-      photo: "https://res.cloudinary.com/dqlgqfxi6/image/upload/v1743444566/nishadeep_ujobp7.jpg",
-      linkedin: "https://www.linkedin.com/in/nishadeeph?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-      category: "Core Team"
-    },
-    {
       name: "Ankush Parihar ",
-      designation: "Media Head",
+      designation: "Secretary",
       photo: "https://res.cloudinary.com/dqlgqfxi6/image/upload/v1743444554/ankush_y8oell.jpg",
       linkedin: "https://www.linkedin.com/in/ankush-parihar-7b0546271?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
       category: "Core Team"
     },
     {
-      name: "Raj Solanki ",
-      designation: "Media Coordinator",
-      photo: "https://res.cloudinary.com/dqlgqfxi6/image/upload/v1743444569/raj_yg1cay.jpg",
-      linkedin: "https://www.linkedin.com/in/raj-solanki-62a5b22a6",
+      name: "Harsh Narayankar",
+      designation: "Treasurer",
+      photo: "https://res.cloudinary.com/dqlgqfxi6/image/upload/v1743444560/harsh_pnsjxd.jpg",
+      linkedin: "https://www.linkedin.com/in/harshnarayankar/",
       category: "Core Team"
     },
+
+
+    // content
+    /*
+    {
+      name: "",
+      designation: "",
+      photo: "",
+      linkedin: "",
+      category: "Core Team"
+    },
+    */
     {
       name: "Shravan Kulkarni ",
-      designation: "Publicity Member",
+      designation: "Content Coordinator",
       photo: "https://res.cloudinary.com/dqlgqfxi6/image/upload/v1743444572/shravan_sfkmp0.jpg",
       linkedin: "https://www.linkedin.com/in/shravan-kulkarni-966586288/",
       category: "Core Team"
     },
+
+
+
+    //media
+    /*
     {
-      name: "Utkarsh Kumar",
-      designation: "Content Coordinator",
-      photo: "https://res.cloudinary.com/dqlgqfxi6/image/upload/v1743444574/utkarsh_o2xl7q.jpg",
+      name: "",
+      designation: "",
+      photo: "",
+      linkedin: "",
+      category: "Core Team"
+    },
+    {
+      name: "",
+      designation: "",
+      photo: "",
+      linkedin: "",
+      category: "Core Team"
+    },
+    {
+      name: "",
+      designation: "",
+      photo: "",
+      linkedin: "",
+      category: "Core Team"
+    },
+    */
+    {
+      name: "Om Kedare",
+      designation: "Video Coordinator",
+      photo: "https://res.cloudinary.com/dqlgqfxi6/image/upload/v1760464228/om_ui1nwj.jpg",
+      linkedin: "https://www.linkedin.com/in/om-kedare-91b4a2244/",
+      category: "Core Team"
+    },
+    
+
+
+    //events
+    {
+      name: "Nishadeep H ",
+      designation: "Event & Publicity Head",
+      photo: "https://res.cloudinary.com/dqlgqfxi6/image/upload/v1743444566/nishadeep_ujobp7.jpg",
+      linkedin: "https://www.linkedin.com/in/nishadeeph?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+      category: "Core Team"
+    },
+    {
+      name: "Jishnu Prasad",
+      designation: "Event & Publicity Coordinator",
+      photo: "https://res.cloudinary.com/dqlgqfxi6/image/upload/v1760464875/jishnu_i0ycyy.jpg",
+      linkedin: "https://www.linkedin.com/in/jishnu-prasad-ba90a9328/",
+      category: "Core Team"
+    },
+    {
+      name: "Srujan R Soori",
+      designation: "Event & Publicity Coordinator",
+      photo: "https://res.cloudinary.com/dqlgqfxi6/image/upload/v1760464653/srujan_ot4wgu.jpg",
+      linkedin: "https://www.linkedin.com/in/srujan-soori/",
+      category: "Core Team"
+    },
+
+
+/*
+    //debate
+    {
+      name: "",
+      designation: "",
+      photo: "",
+      linkedin: "",
+      category: "Core Team"
+    },
+    {
+      name: "",
+      designation: "",
+      photo: "",
+      linkedin: "",
+      category: "Core Team"
+    },
+
+
+
+
+
+    //public relations
+    {
+      name: "",
+      designation: "",
+      photo: "",
+      linkedin: "",
+      category: "Core Team"
+    },
+    {
+      name: "",
+      designation: "",
+      photo: "",
       linkedin: "",
       category: "Core Team"
     },
@@ -149,6 +213,7 @@ export const teamData = [
       linkedin: "https://www.linkedin.com/in/anant-kamat-a19813275/",
       category: "Core Team"
     },
+*/
    
 
     //alumni
